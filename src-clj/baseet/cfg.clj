282fcb7ae5914d694912e1680af1a1d41a92db20
Bassem Youssef <bassem@outlook.com>
2013-07-06 17:04:39 -0700

    Initialization,start and stop.

---
+++ src-clj/baseet/cfg.clj
@@ -0,0 +1,40 @@
+(ns baseet.cfg)
+
+(defn get-twitter-cfg
+  "Get twitter keys and tokens from config file"
+  []
+  (read-string (slurp "twitter-cfg.txt")))
+
+(defrecord DefaultCfg [cfg-file server-params db-params])
+(defrecord DefaultDbParams [db-name db-design-doc db-view])
+(defrecord DefaultServerParams [port])
+
+(defn default-server-params []
+  (->DefaultServerParams 7623))
+
+(defn default-db-params []
+  (map->DefaultDbParams {:db-name "tw-db"
+                         :db-design-doc "tw-design-doc"
+                         :db-view "tw-list-view"
+                         :db-view-key :tw-list-view}))
+
+(defn default-cfg []
+  (map->DefaultCfg {:cfg-file "baseet-cfg.txt"
+                    :server-params (default-server-params)
+                    :db-params (default-db-params)}))
+
+(defn merge-cfg
+  "Read our config file and merge it with the config supplied
+  from the user. User configs overwrite config file settings."
+  [user-cfg]
+  (let [cfg-file (:cfg-file user-cfg)]
+    (if (empty? cfg-file)
+      user-cfg
+      (if (.exists (clojure.java.io/as-file cfg-file))
+        (let [existing-cfg (read-string (slurp cfg-file))]
+          ;; now we merge the exiting config with what the user specified
+          (reduce (fn [acc [k v]]
+                     (if (not (contains? acc k))
+                       (merge acc (hash-map k v))
+                       acc)) user-cfg existing-cfg))
+        user-cfg))))
--- src-clj/baseet/core.clj
+++ src-clj/baseet/core.clj
@@ -1,13 +1,57 @@
 (ns baseet.core
-  (:require [ring.adapter.jetty :only (run-jetty)]
-            [compojure.handler :as h :only (api)] 
-            [baseet.routes :as routes :only (app)]))
+  (:require [baseet.server :as server]
+            [baseet.db :as db]
+            [baseet.cfg :as cfg]
+            [ring.adapter.jetty :as jetty :only (run-jetty)]
+            [compojure.handler :as handler :only (api)]
+            [baseet.routes :as r :only (app)]))  
 
+(defprotocol Lifecycle
+  "A protocol specifying the lifecycle protocol per S.Siera"
+  (start [_] "Starting service")
+  (stop [_] "Stopping service"))
 
+(defrecord WebServer [svr]
+  Lifecycle
+  (start [_] (server/start svr))
+  (stop [_] (server/stop svr)))
 
+(defrecord DbStore [db-params]
+  Lifecycle
+  (start [_] (db/start db-params))
+  (stop [_] (db/stop db-params)))
+
+(defrecord Application [config web-server db-store]
+  Lifecycle
+  (start [_]
+    (start web-server) ;; what to give it ?
+    (start db-store))
+  (stop [_]
+    (stop db-store)
+    (stop web-server)))
+
+(defn db-store-init [config]
+  (->DbStore config))
+
+(defn web-server-init [config]
+  (->WebServer (atom #(jetty/run-jetty (-> r/app handler/api)
+                                        {:port (:port config) :join? false}))))
+
+(defn app
+  "Return an instance of the application"
+  [config]
+  (let [config (cfg/merge-cfg config)
+        db (db-store-init (:db-params config))
+        web-server (web-server-init (:server-params config))]
+    (->Application config web-server db)))
+
+; provide a cli option later to specify config file
+; optionally (def app (atom nil))
 (defn -main
-  ([] (-main 7623))
-  ([port] (run-jetty (-> routes/app h/api) 
-                     {:port (Integer. port) :join? false})))
-
-
+  ([] (-main (cfg/default-cfg)))
+  ([config]
+   (when-not (empty? config)
+     (let [system (app config)]
+       (start system)
+       (.. (Runtime/getRuntime) (addShutdownHook (proxy [Thread] []
+                                                   (run [] (stop system)))))))))

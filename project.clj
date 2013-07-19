3393db5447b8ed523a0f9f7987c41cefe012ed90
Bassem Youssef <bassem@outlook.com>
2013-07-19 00:39:59 -0700

    Forgot to add project.clj

---
+++ project.clj
@@ -0,0 +1,24 @@
+(defproject baseet "0.1.0-SNAPSHOT"
+  :description "Frontend for suweet"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src-clj"]
+  :main baseet.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.2.0-RC1"]
+                 [ring/ring-jetty-adapter "1.2.0-RC1"]
+                 [org.clojure/clojurescript "0.0-1835"]
+                 [com.ashafa/clutch "0.4.0-RC1"]
+                 [compojure "1.1.5"] 
+                 [hiccup "1.0.3"]
+                 [prismatic/dommy "0.1.1"]
+                 [suweet "0.1.4-SNAPSHOT"]]
+  :plugins  [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild  {
+    :builds [{
+          :source-paths ["src-cljs"]
+          :compiler {
+            :output-to "resources/public/js/main.js"
+            :optimizations :whitespace
+            :pretty-print true}}]})

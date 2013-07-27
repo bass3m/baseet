dea3f71f9b0ca5d568ecfbaeb84b787d9122508b
Bassem Youssef <bassem@outlook.com>
2013-07-26 20:02:35 -0700

    Make sure leon min version is 2

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src-clj"]
+  :min-lein-version "2.0.0"
   :main baseet.core
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.0-RC1"]

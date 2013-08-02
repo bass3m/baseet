8944aad21c90b3ac8482947eeef2a46ccf0952f6
Bassem Youssef <bassem@outlook.com>
2013-08-02 13:35:17 -0700

    Some route cleanup

--- src-clj/baseet/routes.clj
+++ src-clj/baseet/routes.clj
@@ -1,5 +1,5 @@
 (ns baseet.routes
-  (:require [compojure.core :refer [GET PUT POST defroutes routes]]
+  (:require [compojure.core :refer [GET PUT POST routes]]
             [compojure.route :as route :only (route)]
             [noir.util.route :refer (restricted)]
             [noir.session :as session :only (get)]
@@ -10,24 +10,6 @@
 (defn logged-in? [_]
   (session/get :user))
 
-;; Define routes:
-;; GET "/lists"    => lists : get our twitter lists
-;; GET "/list/<list-id>
-;;      => get list-tweets : get all highest scoring tweets from a list
-;;         <list-id> : numeric id of twitter list
-;; GET "/list-unread/<list-id>
-;;      => get list-tweets : get unread highest scoring tweets from a list
-;;         <list-id> : numeric id of twitter list
-;; GET "/summarize/<tweet-id>"
-;;      => summarize : get summary for link.
-;;         <tweet-id> : the tweet id containing the url to be summarized
-;; POST "/tweet-read/<tweet-id>"
-;;       => tweet-read : mark tweet as read
-;;         <tweet-id> : the tweet id that we already read
-;; PUT "/list-done/<list-id>"
-;;      => list-done : mark twitter list as read
-;;         <list-id> : the list id that we already read
-;; PUT "/all-done" => all-done : mark all lists read
 (defn app [ctx]
   (routes
     (GET "/" request (c/login request))
@@ -36,11 +18,6 @@
     (GET "/lists" request (restricted (-> request
                                           (m/all-twitter-lists ctx)
                                           v/all-twitter-lists)))
-    (GET ["/list/:id/:list-name" :id #"\d+" :list-name #"\D+"]
-         [& list-req]
-         (restricted (as-> list-req _
-                           (m/a-twitter-list {:option :all :list-req _} ctx)
-                           (v/a-twitter-list _))))
     (GET ["/list-unread/:id/:list-name" :id #"\d+" :list-name #"\D+"]
          [& list-req]
          (restricted (as-> list-req _
@@ -62,9 +39,6 @@
          (restricted (as-> list-req _
                            (m/prev-in-twitter-list {:option :unread :list-req _} ctx)
                            (v/a-twitter-list _))))
-    (PUT "/read-tweet/:id" [id] (restricted (as-> id _
-                                                  (m/mark-tweet-read _ ctx)
-                                                  (v/mark-tweet-read _))))
     (PUT ["/mark-many/:list-name/:start/:end"
           :list-name #"\D+" :start #"\w+" :end #"\w+"]
          [& page-params]
